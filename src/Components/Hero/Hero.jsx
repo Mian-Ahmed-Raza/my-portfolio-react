f5b05596af5e22a0cf5f17ec90cdd693d0b79689
Mian-Ahmed-Raza <ahmedmian026@gmail.com>
2025-09-28 07:17:20 +0500

    Added hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import styles from './Hero.module.css'
+import profilePic from '../../Assets/profile_img.svg'
 
 const Hero = () => {
-  return (
+return (
     <div className={styles.hero}>
 
-        
+            <img src={profilePic} alt="profile" />
+
+            <h1><span>I'm Ahmed Raza,</span> Frontend Developer based in Pakistan</h1>
+
+            <p>Passionate about building interactive and responsive web applications using modern technologies.</p>
+
+            <div className={styles.heroAction}>
+
+                <div className={styles.heroConnect}>Connect with me</div>
+
+                <div className={styles.heroResume}>My Resume</div>
+
+            </div>
 
     </div>
-  )
+)
 }
 
 export default Hero
