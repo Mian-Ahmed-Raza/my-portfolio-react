e4cbee0ae86a9abc39516ebd0dc56df701ed269a
Mian-Ahmed-Raza <ahmedmian026@gmail.com>
2025-09-28 07:00:17 +0500

    added navbar

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Hero.module.css'
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+
+        
+
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './Navbar.module.css'
+import logo from '../../assets/logo.svg'
+
+export default function Navbar() {
+  return (
+
+    <div className={styles.navbar}>
+
+        <img src={logo} alt="" />
+
+        <ul className={styles.navMenu}>
+
+            <li>Home</li>
+            <li>About me</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+
+        </ul>
+
+        <div className={styles.navContact}>
+
+            Connect with me
+
+        </div>
+
+    </div>
+  )
+
+}
