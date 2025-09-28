aa66e10d824f585ba83c8ff2cdd656977c717225
Mian-Ahmed-Raza <ahmedmian026@gmail.com>
2025-09-28 08:27:46 +0500

    added my work section

---
+++ src/Components/MyWork/MyWork.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import styles from './MyWork.module.css'
+import theme_pattern from '../../Assets/theme_pattern.svg'
+import mywork_data from '../../Assets/mywork_data'
+import arrow_icon from '../../Assets/arrow_icon.svg'
+
+const MyWork = () => {
+
+  return (
+    
+    <div className={styles.myWork}>
+
+        <div className={styles.myworkTitle}>
+
+            <h1>My Latest Work</h1>
+
+            <img src={theme_pattern} alt="theme pattern" />
+
+        </div>
+
+        <div className={styles.myworkContainer}>
+
+            {mywork_data.map((work, index) => {
+
+                return <img key={index} src={work.w_img} alt={work.w_name} />
+
+            })}
+
+        </div>
+
+        <div className={styles.myworkShowmore}>
+
+            <p>Show More</p>
+            <img src={arrow_icon} alt="arrow icon" />
+
+        </div>
+
+    </div>
+
+  )
+
+}
+
+export default MyWork
